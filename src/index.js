58ad86a6e632383239997cd6c5dc708670599cee
Zaheer khan,Zaheer Khan, Habib khan, Habib Khan <zaheerkhan872004@gmail.com,zaheer.224246108@vcet.edu.in,habibkhan15031999@gmail.com,habibkhan19992022@gmail.com>
2024-03-17 07:28:18 +0530

    prepared model of User

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require ('express');
+// const dotenv = require('dotenv');
+// dotenv.config();
 const app = express();
 const { PORT } = require('./config/serverConfig');
+// const PORT = process.env.PORT
 
-const prepareAndStartService = () =>{
+
+
+const prepareAndStartService = () => {
     app.listen(PORT,()=>{
 
         console.log(`Server started at ${PORT} port`);
