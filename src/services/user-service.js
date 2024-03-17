89c06fea2aeb1fb7d6374078044dd027aa07558a
Zaheer khan,Zaheer Khan, Habib khan, Habib Khan <zaheerkhan872004@gmail.com,zaheer.224246108@vcet.edu.in,habibkhan15031999@gmail.com,habibkhan19992022@gmail.com>
2024-03-17 11:25:44 +0530

    Added jwt token creation and verify logic in login

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const { JWT_KEY} = require('../config/serverConfig');
+
 const UserRepository = require('../repository/user-repository');
 
 class UserService{
@@ -13,6 +16,26 @@ class UserService{
         } catch (error) {
             console.log(`Something went wrong in service-layer`);
             throw error;
+        }
+    }
+
+    createToken(user){
+        try {
+            const result = jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+            return result;
+        } catch (error) {
+            console.log(`Something went wrong in token generation`);
+            throw error;
+        }
+    }
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log(`Something went wrong in token validation`);
+            throw error;
+            
         }
     }
 
